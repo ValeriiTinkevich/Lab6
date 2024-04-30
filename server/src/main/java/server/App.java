e93b815ee2081474da26a25cbba81bd403e29c8f
Valerii Tinkevich <barm2001@gmail.com>
2024-04-30 23:12:37 +0300

    Pushed lab 6

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -9,6 +9,7 @@ import server.utility.RequestHandler;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
 public class App {
@@ -19,6 +20,10 @@ public class App {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setEncoding("UTF-8");
+        logger.setUseParentHandlers(false);
+        logger.addHandler(consoleHandler);
         String fileName = "C:\\Users\\User\\Documents\\lab6\\server\\src\\main\\java\\server\\data.csv";
         FileManager fileManager = new FileManager(fileName, ";");
         CollectionManager collectionManager = new CollectionManager(fileManager);
